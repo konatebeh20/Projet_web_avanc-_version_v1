8611e8974ae158ccafe6b8b0873b490ad560c686
Konate beh <konatebeh20@gmail.com>
2024-11-21 11:02:13 +0000

    Update test.py

--- clients/test.py
+++ clients/test.py
@@ -1,6 +1,6 @@
 import requests
 
-endpoint = "http://localhost:8000/api/?q=konatebeh20programmeur"
+endpoint = "http://localhost:8000/api/?q=konateprogrammeur"
 
 response = requests.get(endpoint)
 
